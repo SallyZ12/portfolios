32d22bd51b17d9fbd9204290c618fa1f5be4918f
github username <github email address>
2018-06-30 14:35:41 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+
+  get '/login' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   resources :users
+  resources :credits
+  resources :transactions
+  resources :exposures
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
