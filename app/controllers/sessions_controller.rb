dd5b61b4df0dea4296de405470666662f7b6d69a
github username <github email address>
2018-07-04 22:55:32 -0400

    validation notice for log in, removed password_confirmation from login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
           session[:user_id] = user.id
             redirect_to user_path(user)
           else
+            flash[:notice] = "Log In Failed- Try Again or Register"
            redirect_to '/'
       end
     end
