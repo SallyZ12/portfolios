b06464054de15276939ef40a88f59891ee68899c
github username <github email address>
2018-07-07 13:07:18 -0400

    refactored for correct error code for field_with_errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,7 @@ class UsersController < ApplicationController
        session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
-        # flash[:notice] = "Inputs Not Valid, Try Again"
-        redirect_to new_user_path
+        render :new
       end
     end
 
