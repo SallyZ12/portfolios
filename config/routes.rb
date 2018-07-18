ba23ee337ebbb3be1bcfe47c7c546da55ac64f71
github username <github email address>
2018-07-18 16:52:17 -0400

    added a new action destroy

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :exposures do
-    resources :transactions, only: [:new, :create, :show, :index, :edit, :update]
+    resources :transactions, only: [:new, :create, :show, :index, :edit, :update, :destroy]
   end
 
 
