768a793cd7e6b43a7c5a26823424751cec66f7b5
github username <github email address>
2018-08-03 19:12:17 -0400

    added :email to strong params, adjusted log_in form, changed a user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
     private
 
       def user_params
-        params.require(:user).permit(:username, :password, :password_confirmation, :insurer)
+        params.require(:user).permit(:username, :password, :password_confirmation, :insurer, :email)
       end
 
       def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :username, uniqueness: true
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
   validates :password_digest, presence: true
 
 
