16914bd92a48539c489667d27d368108a5d19cac
github username <github email address>
2018-08-03 18:35:15 -0400

    added email attribute to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_01_190859) do
+ActiveRecord::Schema.define(version: 2018_08_03_223355) do
 
   create_table "credits", force: :cascade do |t|
     t.string "credit_name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_07_01_190859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "insurer", default: false
+    t.string "email"
   end
 
 end
