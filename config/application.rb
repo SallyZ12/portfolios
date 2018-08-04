5c3ac3510e89af4f93ef0c613397173de50babab
github username <github email address>
2018-08-04 12:08:40 -0400

    moved link for github login to layout

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Portfolio
     config.load_defaults 5.2
 
      # for Github required https
-    config.force_ssl = true
+    # config.force_ssl = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
