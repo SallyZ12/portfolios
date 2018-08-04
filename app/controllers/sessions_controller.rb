f815fe10878ad90f27aee400a2024e0cb154433f
github username <github email address>
2018-08-04 14:13:59 -0400

    still working on github login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     def create
        if auth_hash = request.env['omniauth.auth']
         user = User.find_or_create_by_omni_auth(auth_hash)
+
             session[:user_id] = user.id
               redirect_to user_path(user)
 
