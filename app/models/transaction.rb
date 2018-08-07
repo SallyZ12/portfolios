747b15d82224f6d74691b933cec220c271c44a41
github username <github email address>
2018-08-07 18:37:51 -0400

    still working on validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,11 @@
 class Transaction < ApplicationRecord
 
   belongs_to :exposure
-  
+
 
   validates :name, presence: true
   validates :series, presence: true
-  validates_uniqueness_of :series, scope: :credit_id
+  validates_uniqueness_of :series, scope: :exposure_id
 
 
   scope :trans_top_ten, -> {order("transactions.par DESC").limit(10)}
