d1c01f828479ef21c278a7cdfdc1acf75860b782
github username <github email address>
2019-02-13 15:18:01 -0500

    changed redirect of login to exposure index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,9 @@ class SessionsController < ApplicationController
                user = User.find_by(email: params[:user][:email])
              if user && user.authenticate(params[:user][:password])
                session[:user_id] = user.id
-                 redirect_to user_path(user)
+               # for use in js project
+                  redirect_to exposures_path
+                 # redirect_to user_path(user)
                else
                  flash[:notice] = "Log In Failed- Try Again or Register"
                   redirect_to '/'
