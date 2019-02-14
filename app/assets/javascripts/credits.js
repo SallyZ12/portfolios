e5f35c1183a316905265b3ad02840ed3872741e6
github username <github email address>
2019-02-14 15:27:39 -0500

    working on post function for new credits in js file

--- app/assets/javascripts/credits.js
+++ app/assets/javascripts/credits.js
@@ -1,17 +1,22 @@
 
-  // $(function (){
-  //   $('form').submit(function(event) {
-  //     event.preventDefault();
-  //
-  //       let inputs = $(this).serialize();
-  //       let addCredit = $.post('/credits', inputs);
-  //
-  //       addCredit.done(function(data) {
-  //         let credit = data;
-  //         $("#creditName").text(credit["credit_name"]);
-  //         $("#creditSector").text(credit["sector"]);
-  //         $("#creditRating").text(credit["rating"]);
-  //         $("#creditState").text(credit["state"]);
-  //       });
-  //   });
-  // });
+$(function() {
+  addCredit()
+});
+
+
+  function addCredit(){
+    $('form#new-credit').submit(function(event) {
+      event.preventDefault();
+
+        let inputs = $(this).serialize();
+        let addCredit = $.post('/credits', inputs);
+
+        addCredit.done(function(data) {
+          let credit = data;
+          $("#creditName").text(credit["credit_name"]);
+          $("#creditSector").text(credit["sector"]);
+          $("#creditRating").text(credit["rating"]);
+          $("#creditState").text(credit["state"]);
+        });
+    });
+  };
