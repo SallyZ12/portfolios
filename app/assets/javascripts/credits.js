fd7df8a281951d14004b0eae9a16aaad0b568e33
github username <github email address>
2019-02-13 20:46:15 -0500

    commented out js until figure out event listener to prevent load before click

--- app/assets/javascripts/credits.js
+++ app/assets/javascripts/credits.js
@@ -1,17 +1,17 @@
 
-  $(function (){
-    $('form').submit(function(event) {
-      event.preventDefault();
-
-        let inputs = $(this).serialize();
-        let addCredit = $.post('/credits', inputs);
-
-        addCredit.done(function(data) {
-          let credit = data;
-          $("#creditName").text(credit["credit_name"]);
-          $("#creditSector").text(credit["sector"]);
-          $("#creditRating").text(credit["rating"]);
-          $("#creditState").text(credit["state"]);
-        });
-    });
-  });
+  // $(function (){
+  //   $('form').submit(function(event) {
+  //     event.preventDefault();
+  //
+  //       let inputs = $(this).serialize();
+  //       let addCredit = $.post('/credits', inputs);
+  //
+  //       addCredit.done(function(data) {
+  //         let credit = data;
+  //         $("#creditName").text(credit["credit_name"]);
+  //         $("#creditSector").text(credit["sector"]);
+  //         $("#creditRating").text(credit["rating"]);
+  //         $("#creditState").text(credit["state"]);
+  //       });
+  //   });
+  // });
