adaf4e19dc14e93229289971703f2a1db4a85241
github username <github email address>
2019-02-15 07:48:40 -0500

    still attempting to move js code from _form view to js for receiving api posts

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,7 @@
 $(function() {
   console.log("index.js loaded")
   getExposures()
+  newCredit()
 });
 
 function getExposures(){
@@ -35,3 +36,21 @@ class Exposure{
 // custom function on the prototypee of Exposure
 // create some custom HTML
 // append theml
+
+
+function newCredit(){
+  $('form#new-credit').on('submit', (function(event){
+    event.preventDefault();
+
+      let inputs = $(this).serialize();
+      let addCredit = $.post('/credits', inputs);
+
+      addCredit.done(function(data) {
+        let credit = data;
+        $("#creditName").text(credit["credit_name"]);
+        $("#creditSector").text(credit["sector"]);
+        $("#creditRating").text(credit["rating"]);
+        $("#creditState").text(credit["state"]);
+      });
+  }));
+};
