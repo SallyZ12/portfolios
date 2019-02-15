ec77f43fd464363f912d561032a6e8e79c11701c
github username <github email address>
2019-02-14 19:55:56 -0500

    still trying to get ajax response moving js to credit.js file from _form

--- app/assets/javascripts/credits.js
+++ app/assets/javascripts/credits.js
@@ -1,22 +1,17 @@
 
-$(function() {
-  addCredit()
-});
-
-
-  function addCredit(){
-    $('form#new-credit').submit(function(event) {
-      event.preventDefault();
-
-        let inputs = $(this).serialize();
-        let addCredit = $.post('/credits', inputs);
-
-        addCredit.done(function(data) {
-          let credit = data;
-          $("#creditName").text(credit["credit_name"]);
-          $("#creditSector").text(credit["sector"]);
-          $("#creditRating").text(credit["rating"]);
-          $("#creditState").text(credit["state"]);
-        });
-    });
-  };
+  // $(function(){
+  //   $('form#new-credit').submit(function(event) {
+  //     event.preventDefault();
+  //
+  //       let inputs = $(this).serialize();
+  //       let addCredit = $.post('/credits', inputs);
+  //
+  //       addCredit.done(function(data) {
+  //         let credit = data;
+  //         $("#creditName").text(credit["credit_name"]);
+  //         $("#creditSector").text(credit["sector"]);
+  //         $("#creditRating").text(credit["rating"]);
+  //         $("#creditState").text(credit["state"]);
+  //       });
+  //   });
+  // });
