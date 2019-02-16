5ee9e1e5dda086a781b52de0ce77d566464775f4
github username <github email address>
2019-02-16 15:14:18 -0500

    uncommented render partial in new action

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -23,7 +23,7 @@ class CreditsController < ApplicationController
   def new
     @user = User.find(params[:user_id])
       @credit = Credit.new
-          # render partial: 'ajax_new'
+          render partial: 'ajax_new'
   end
 
   def create
@@ -36,7 +36,7 @@ class CreditsController < ApplicationController
           else
             @credit = Credit.new(credit_params)
               if @credit.save
-                @exposure = Exposure.find_or_create_by(user_id: current_user.id, credit_id: @credit.id)            
+                @exposure = Exposure.find_or_create_by(user_id: current_user.id, credit_id: @credit.id)
                   render json: @credit, status: 201
                   # redirect_to credit_path(@credit)
                     flash[:message] = "Credit Successfully Created"
