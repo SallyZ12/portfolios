9b5fd13ae58dece9cba0ebebb1cdc0a1ca20a0be
github username <github email address>
2019-02-18 12:30:07 -0500

    working on retrieving json data and inserting into DOM on page

--- app/assets/javascripts/exposures.js
+++ app/assets/javascripts/exposures.js
@@ -12,23 +12,68 @@ function getExposures(){
       url: 'http://localhost:3000/exposures',
       method: 'get',
       dataType: 'json'
-    }).done(function(response){
-      console.log('response: ', response)
+    }).done(function(data){
+      console.log('response:',data)
+      let myexposure = new Exposure(data[1])
+      let myExposureHTML = myexposure.postHTML()
+      document.getElementById("exposure-data").innerHTML += myExposureHTML
 
     })
-
   })
-
 }
 
 class Exposure{
   constructor(obj){
-    this.user_id = obj.user_id;
-    this.credit_id = obj.credit_id;
+    this.id = obj.id
+    this.credit_id = obj.credit_id
+    this.user_id = obj.user_id
     this.limit = obj.limit;
     this.rating = obj.rating;
-  }
-}
+    // if(user){
+    //   this.userId = user.id
+    // }
+    //
+    //   setUser(user){
+    //     this.userId = user.id;
+    //   }
+    }
+  };
+
+// class User {
+//   constructor(obj) {
+//     this.username = obj.username
+//   }
+// }
+//
+// class Transaction {
+//   constructor(obj) {
+//     this.name = obj.name
+//     this.series = obj.series
+//
+//   }
+// }
+
+
+
+
+// class Credit{
+//   constructuor(obj){
+//     this.credit_name = obj.credit_name
+//     this.rating = obj.rating
+//     this.sector = obj.sector
+//     this.state = obj.state
+//   }
+// }
+
+Exposure.prototype.postHTML = function (){
+  return (`
+    <div>
+     <p> Exposure ID: ${this.id} </p>
+     <p> Exposure Limit: ${this.limit} </p>
+     <p> Company Rating: ${this.rating} </p>
+    </div>
+  `)
+};
 
 // Exposure.prot
 
@@ -37,21 +82,3 @@ class Exposure{
 // custom function on the prototypee of Exposure
 // create some custom HTML
 // append theml
-
-
-// function newCredit(){
-//   $('add-credit form').on('submit', (function(event){
-//     event.preventDefault();
-//
-//       let inputs = $(this).serialize();
-//       let addCredit = $.post('/credits', inputs);
-//
-//       addCredit.done(function(data) {
-//         let credit = data;
-//         $("#creditName").text(credit["credit_name"]);
-//         $("#creditSector").text(credit["sector"]);
-//         $("#creditRating").text(credit["rating"]);
-//         $("#creditState").text(credit["state"]);
-//       });
-//   }));
-// };
