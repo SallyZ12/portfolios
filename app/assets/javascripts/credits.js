cf5a585e7999e8a400a387cf26b837bd3b9886f5
github username <github email address>
2019-02-19 18:54:10 -0500

    figured out how to post credit with ajax response

--- app/assets/javascripts/credits.js
+++ app/assets/javascripts/credits.js
@@ -33,13 +33,47 @@ $(function() {
           let addCredit = $.post('/credits', inputs);
 
           addCredit.done(function(data) {
-            let credit = data;
             // use Credit Class here
+              let mycredit = new Credit(data)
+              let myCreditHTML = mycredit.creditHTML()
+              document.getElementById("new_credit").innerHTML += myCreditHTML
+            })
+            })
+          };
 
-            $("#creditName").text(credit["credit_name"]);
-            // $("#creditSector").text(credit["sector"]);
-            // $("#creditRating").text(credit["rating"]);
-            // $("#creditState").text(credit["state"]);
-          });
-      });
+
+    class Credit{
+      constructor(obj){
+        this.id = obj.id;
+        this.credit_name = obj.credit_name;
+        this.rating = obj.rating;
+        this.sector = obj.sector;
+        this.state = obj.state;
+      }
+    };
+
+
+    Credit.prototype.creditHTML = function (){
+
+    return (`
+        <table>
+        <caption> <h4>New Credit Response </h4></caption>
+          <thead>
+          <tr>
+          <th>Credit Name</th>
+          <th>Sector </th>
+          <th>State </th>
+          <th>Ext Rating</th>
+          </tr>
+          </thead>
+            <tbody>
+              <tr>
+              <td> ${this.credit_name} </td>
+              <td> ${this.sector} </td>
+              <td> ${this.state} </td>
+              <td> ${this.rating} </td>
+            </tr>
+            </tbody>
+        </table>
+      `)
     };
